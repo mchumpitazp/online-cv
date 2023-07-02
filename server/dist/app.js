8c28354beb7f20e046386082563517053387deca
Mauro Chumpitaz Polino <mchumpitazp@gmail.com>
2023-07-02 23:21:22 +0300

    cms init

--- server/dist/app.js
+++ server/dist/app.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // modules
 require('dotenv').config();
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 const path = require('path');
 // const cors = require('cors');
 const helmet = require('helmet');
@@ -18,8 +19,24 @@ app.listen(port, () => {
     connectDB();
 });
 // tools
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(helmet());
 // app.use(cors());
 // routers
 app.use('/api/projects', projectRouter);
+// CRUD DEV
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayout);
+app.set('layout', './layout');
+app.set('view engine', 'ejs');
+// home 
+app.get('/', (req, res) => {
+    res.render('index');
+    const locals = {
+        title: 'Admin',
+        description: 'CRUD NODEJS'
+    };
+});
--- server/src/app.tsx
+++ server/src/app.tsx
@@ -1,6 +1,9 @@
+import { Response } from "express";
+
 // modules
 require('dotenv').config();
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 const path = require('path');
 // const cors = require('cors');
 const helmet = require('helmet');
@@ -19,11 +22,33 @@ app.listen(port, () => {
 })
 
 // tools
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(helmet());
 // app.use(cors());
 
 // routers
 app.use('/api/projects', projectRouter);
+
+
+
+// CRUD DEV
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayout);
+app.set('layout', './layout');
+app.set('view engine', 'ejs');
+
+// home 
+app.get('/', (req: any, res: any) => {
+    res.render('index');
+    
+    const locals = {
+        title: 'Admin',
+        description: 'CRUD NODEJS'
+    }
+})
 
 export {};
